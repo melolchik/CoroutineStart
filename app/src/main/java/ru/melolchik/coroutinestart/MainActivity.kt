feb14a6bec674a4fb4a44a3865bcb098f0ae7763
Olga Melekhina <o.melehina@smart-fin.ru>
2024-11-15 19:02:47 +0300

    8.3 Async programming problems

--- app/src/main/java/ru/melolchik/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/melolchik/coroutinestart/MainActivity.kt
@@ -1,6 +1,8 @@
 package ru.melolchik.coroutinestart
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,39 +32,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun loadData() {
+        Log.d("MainActivity", "Load started: $this")
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
-        loadCity{city : String ->
-            binding.tvLocation.text = city
-             loadTemperature(city){
+        loadCity {
+            binding.tvLocation.text = it
+            loadTemperature(it) {
                 binding.tvTemperature.text = it.toString()
                 binding.progress.isVisible = false
                 binding.buttonLoad.isEnabled = true
+                Log.d("MainActivity", "Load finished: $this")
             }
-
         }
-
     }
 
-    private fun loadCity(callback : (String) -> Unit) {
+    private fun loadCity(callback: (String) -> Unit) {
         thread {
             Thread.sleep(5000)
-            callback.invoke("Moscow")
+            runOnUiThread {
+                callback.invoke("Moscow")
+            }
         }
-
     }
 
-    private fun loadTemperature(city : String,callback: (Int) -> Unit){
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Toast.makeText(
-                this,
-                getString(R.string.loading_temperature_toast, city),
-                Toast.LENGTH_SHORT
-            ).show()
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
             Thread.sleep(5000)
-           callback.invoke(17)
+            runOnUiThread {
+                callback.invoke(17)
+            }
         }
-
     }
 }
