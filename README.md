86e09fde5c7dabdd2cde90b90b1f53d0ec2ef8d0
Olga Melekhina <o.melehina@smart-fin.ru>
2024-12-18 13:21:12 +0300

    14.1 Job and Coroutine Builders

--- README.md
+++ README.md
@@ -4,3 +4,44 @@
 #8.3 Проблемы при стандартном подходе к асинхронному программированию
 #8.4 Введение в Kotlin Coroutines. Suspend функции
 #8.5 Корутины "под капотом"
+
+#14.1 Job and Coroutine Builders
+
+Поменяем loadTemperature , чтобы не зависело от города
+И теперь loadCity и loadTemperature можно запускать одноврменно
+Для этого их нужно выполнять в разных корутинах
+
+ binding.buttonLoad.setOnClickListener {
+            binding.progress.isVisible = true
+            binding.buttonLoad.isEnabled = false
+            lifecycleScope.launch {
+                val city = loadCity()
+                binding.tvLocation.text = city
+            }
+            lifecycleScope.launch {
+                val temp = loadTemperature()
+                binding.tvTemperature.text = temp.toString()
+            }
+        }
+		
+		lifecycleScope.launch - возвращает Job
+		
+binding.buttonLoad.setOnClickListener {
+            binding.progress.isVisible = true
+            binding.buttonLoad.isEnabled = false
+            val jobCity =  lifecycleScope.launch {
+                val city = loadCity()
+                binding.tvLocation.text = city
+            }
+            val jobTemp =  lifecycleScope.launch {
+                val temp = loadTemperature()
+                binding.tvTemperature.text = temp.toString()
+            }
+
+            lifecycleScope.launch {
+                jobCity.join() - ожидание
+                jobTemp.join() - ожидание
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
--- app/src/main/java/ru/melolchik/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/melolchik/coroutinestart/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import ru.melolchik.coroutinestart.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
@@ -32,10 +33,23 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         binding.buttonLoad.setOnClickListener {
-            lifecycleScope.launch {
-                loadData()
+            binding.progress.isVisible = true
+            binding.buttonLoad.isEnabled = false
+            val jobCity =  lifecycleScope.launch {
+                val city = loadCity()
+                binding.tvLocation.text = city
             }
-//            loadWithoutCoroutine()
+            val jobTemp =  lifecycleScope.launch {
+                val temp = loadTemperature()
+                binding.tvTemperature.text = temp.toString()
+            }
+
+            lifecycleScope.launch {
+                jobCity.join()
+                jobTemp.join()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
         }
     }
 
@@ -46,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         val city = loadCity()
 
         binding.tvLocation.text = city
-        val temp = loadTemperature(city)
+        val temp = loadTemperature()
 
         binding.tvTemperature.text = temp.toString()
         binding.progress.isVisible = false
@@ -88,7 +102,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private suspend fun loadCity(): String {
-        delay(5000)
+        delay(2000)
         return "Moscow"
     }
 
@@ -104,12 +118,12 @@ class MainActivity : AppCompatActivity() {
         }, 5000)
     }
 
-    private suspend fun loadTemperature(city: String): Int {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
+    private suspend fun loadTemperature(): Int {
+//        Toast.makeText(
+//            this,
+//            getString(R.string.loading_temperature_toast, city),
+//            Toast.LENGTH_SHORT
+//        ).show()
         delay(5000)
         return 17
     }
