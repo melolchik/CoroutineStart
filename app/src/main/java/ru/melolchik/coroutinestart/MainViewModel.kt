9942673267f5d4c1f34e6275e873babf217c3bd0
Olga Melekhina <o.melehina@smart-fin.ru>
2025-03-24 12:31:16 +0300

    15.1 Coroutine flow started

--- app/src/main/java/ru/melolchik/coroutinestart/MainViewModel.kt
+++ app/src/main/java/ru/melolchik/coroutinestart/MainViewModel.kt
@@ -13,41 +13,29 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-    private val exceptionHandler = CoroutineExceptionHandler{
-        _, throwable -> Log.d(LOG_TAG, "OnExceptionHandler $throwable")
-
+suspend fun Int.isPrime() : Boolean{
+    if(this <= 1) return false
+    for(i in 2 .. this/2){
+        delay(50)
+        if(this % i == 0) return false
     }
+    return true
+}
+class MainViewModel : ViewModel() {
 
-    fun method(){
-       val job =  viewModelScope.launch (Dispatchers.Default + exceptionHandler){
-        Log.d(LOG_TAG, "Started ")
-            val before = System.currentTimeMillis()
-            var count = 0
-            for (i in 0 until 100_000_000){
-                for (j in 0 until 100){
-//                    if(isActive) {
-//                        count++
-//                    }else{
-//                        throw CancellationException()
-//                    }
-                    ensureActive()
-                    count++
-                }
-            }
-            Log.d(LOG_TAG, "Finished ${System.currentTimeMillis() - before}")
-        }
-        job.invokeOnCompletion {
-            Log.d(LOG_TAG, "Coroutine was canceled $it")
-        }
-        viewModelScope.launch {
-            delay(3000)
-            job.cancel()
 
-        }
+     fun method(){
+         viewModelScope.launch {
+             val numbers = listOf(3, 4, 5, 98, 19, 48, 25, 32).asFlow()
+             numbers.filter { it.isPrime() }
+                 .filter { it < 20 }
+                 .collect { Log.d(LOG_TAG, it.toString()) }
+         }
     }
 
 
